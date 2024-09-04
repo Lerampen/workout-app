a592984a37b90ab832e85210473e1fd2e70d670b
Lera <stacyapeyon@gmail.com>
2024-09-04 05:58:32 +0300

    Added Image for exercise illustration for user and the no. of exercise repetitions

--- app/src/main/java/com/example/workoutapp/screens/workouts/ExerciseDetail.kt
+++ app/src/main/java/com/example/workoutapp/screens/workouts/ExerciseDetail.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -23,12 +25,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
 import com.example.workoutapp.R
 import com.example.workoutapp.data.Exercise
 import com.example.workoutapp.ui.theme.robotoFontFamily
@@ -49,7 +54,7 @@ fun ExerciseDetail(
 
 //    Timer State
     var timeLeft by remember {
-        mutableIntStateOf(30)
+        mutableIntStateOf(60)
     }
 //    Manage Timer State: Use a state variable to track whether the timer is running.
     var isTimerRunning by remember {
@@ -64,7 +69,7 @@ fun ExerciseDetail(
         }
     fun stopTimer(){
         isTimerRunning = false
-        timeLeft = 30 // reset timer if needed
+        timeLeft = 60 // reset timer if needed
     }
 
 //    Play Audio function
@@ -103,8 +108,22 @@ fun ExerciseDetail(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        AsyncImage(
+            model = exercise?.exerciseIllustration,
+            contentDescription = exercise?.exerciseName,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .size(200.dp)
+                .clip(RoundedCornerShape(8.dp))
+        )
+
         Text(
             text = "Exercise : ${exercise?.exerciseName}",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Medium
+        )
+        Text(
+            text = " ${exercise?.repetitions} : reps",
             fontSize = 24.sp,
             fontWeight = FontWeight.Medium
         )
